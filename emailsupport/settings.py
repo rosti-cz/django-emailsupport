7ee8446daa5c521c928740122dd1831863e1ca07
rbas <rbas.cz@gmail.com>
2014-10-23 10:17:32 +0200

    Create app settings

---
+++ emailsupport/settings.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+
+
+EMAIL_HOST = getattr(settings, 'EMAIL_HOST', None)
+EMAIL_HOST_PASSWORD = getattr(settings, 'EMAIL_HOST_PASSWORD', None)
+EMAIL_HOST_USER = getattr(settings, 'EMAIL_HOST_USER', None)
+EMAIL_PORT = getattr(settings, 'EMAIL_PORT', None)
+EMAIL_USE_SSL = getattr(settings, 'EMAIL_USE_SSL', False)
