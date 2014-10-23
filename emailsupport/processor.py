95fa3ce2872d0bf86e17a2077071c5073abc8961
rbas <rbas.cz@gmail.com>
2014-10-23 15:00:44 +0200

    Notify users after download new emails

--- emailsupport/processor.py
+++ emailsupport/processor.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.conf import settings
+
 from emailsupport.utils.imap import get_unread_messages, mark_seen
+from emailsupport.settings import NOTIFICATION_EMAIL_LIST
+from emailsupport.tasks import send_mail
 
 from models import Email, Submitter
 
 
 def sanitize_email(email):
     return email
+
+
+def notify_users(email):
+    content = email.get_body()
+    send_mail(email.subject, content, settings.DEFAULT_FROM_EMAIL, NOTIFICATION_EMAIL_LIST)
 
 
 def get_submitter_by_email(email, name):
@@ -35,8 +45,12 @@ def download_and_save():
             email.body_html = message.body['html'][-1]
         except IndexError:
             pass
+
         email_list.append(email)
         uid_list.append(uid)
 
     Email.objects.bulk_create(email_list)
     mark_seen(uid_list)
+
+    for email in email_list:
+        notify_users(email)
--- emailsupport/settings.py
+++ emailsupport/settings.py
@@ -8,3 +8,5 @@ EMAIL_HOST_PASSWORD = getattr(settings, 'EMAIL_HOST_PASSWORD', None)
 EMAIL_HOST_USER = getattr(settings, 'EMAIL_HOST_USER', None)
 EMAIL_PORT = getattr(settings, 'EMAIL_PORT', None)
 EMAIL_USE_SSL = getattr(settings, 'EMAIL_USE_SSL', False)
+
+NOTIFICATION_EMAIL_LIST = getattr(settings, 'NOTIFICATION_EMAIL_LIST', None)
