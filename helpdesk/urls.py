7d34b407a35fe917e919fc01b3a6c736a7bdc372
rbas <rbas.cz@gmail.com>
2014-10-23 13:00:40 +0200

    Remove admin prefix from url

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'helpdesk.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include(admin.site.urls)),
 )
